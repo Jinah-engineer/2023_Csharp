4e484f02673bbec873c7f55d4dc7070e1ca8ee2c
Jinah-engineer <jeeeengineer@gmail.com>
2023-02-05 17:35:56 +0900

    20230205 1st commit

--- ConstraintOnTypeParameters/Program.cs
+++ ConstraintOnTypeParameters/Program.cs
@@ -60,24 +60,49 @@ namespace ConstraintOnTypeParameters
             a.Array[1] = 1;
             a.Array[2] = 2;
 
+            for (int i = 0; i < a.Array.Length; i++)
+            {
+                Console.WriteLine("StructArray<int> : " + $"{a.Array[i]}");
+            }
+
             RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
             b.Array[0] = new StructArray<double>(5);
             b.Array[1] = new StructArray<double>(10);
             b.Array[2] = new StructArray<double>(1005);
+
+            for (int i = 0; i < b.Array.Length; i++)
+            {
+                Console.WriteLine("RefArray<StructArray<double>> : " + $"{b.Array[i].Array.Length}");
+            }
 
             BaseArray<Base> c = new BaseArray<Base>(3);
             c.Array[0] = new Base();
             c.Array[1] = new Derived();
             c.Array[2] = CreateInstance<Base>();
 
+            for (int i = 0; i < c.Array.Length; i++)
+            {
+                Console.WriteLine("BaseArray<Base> : " + $"{c.Array[i]}");
+                Console.WriteLine("BaseArray<Base> Comparisom : " + $"{c.Array[0].Equals(c.Array[2])}");
+            }
+
             BaseArray<Derived> d = new BaseArray<Derived>(3);
             d.Array[0] = new Derived(); // Base 형식은 여기에 할당할 수 없다
             d.Array[1] = CreateInstance<Derived>();
             d.Array[2] = CreateInstance<Derived>();
 
+            for (int i = 0; i < d.Array.Length; i++)
+            {
+                Console.WriteLine("BaseArray<Derived> : " + $"{d.Array[i]}");
+            }
+
             BaseArray<Derived> e = new BaseArray<Derived>(3);
             e.CopyArray<Derived>(d.Array);
 
+            for (int i = 0; i < e.Array.Length; i++)
+            {
+                Console.WriteLine("BaseArray<Derived> (Copied) : " + $"{e.Array[i]}");
+            }
         }
     }
 }
