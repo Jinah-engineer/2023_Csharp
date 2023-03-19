8e64dfdc724a19b6b35bc4d1703087e07e9db649
Jinah-engineer <jeeeengineer@gmail.com>
2023-03-19 22:37:24 +0900

    [2023.03.19]

---
+++ GroupBy/MainApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){ Name="윤현우", Height = 184 },
+                new Profile(){ Name="윤도영", Height = 168 },
+                new Profile(){ Name="강이연", Height = 167 },
+                new Profile(){ Name="이석현", Height = 183 },
+                new Profile(){ Name="서경원", Height = 171 }
+            };
+
+            var listProfile = from profile in arrProfile
+                              orderby profile.Height
+                              group profile by profile.Height < 175 into g
+                              select new { GroupKey = g.Key, Profiles = g };
+
+            foreach (var Group in listProfile)
+            {
+                Console.WriteLine($"- 175cm 미만? : {Group.GroupKey}");
+
+                foreach (var profile in Group.Profiles)
+                {
+                    Console.WriteLine($">>> {profile.Name}, {profile.Height}");
+                }
+            }
+        }
+    }
+}
---
+++ SimpleLinq/MainApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLinq
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){ Name="윤현우", Height = 184 },
+                new Profile(){ Name="윤도영", Height = 168 },
+                new Profile(){ Name="강이연", Height = 167 },
+                new Profile(){ Name="이석현", Height = 183 },
+                new Profile(){ Name="서경원", Height = 171 }
+            };
+
+            var profiles = from profile in arrProfile
+                           where profile.Height < 175
+                           orderby profile.Height
+                           select new
+                           {
+                               Name = profile.Name,
+                               InchHeight = profile.Height * 0.393
+                           };
+
+            foreach (var profile in profiles)
+                Console.WriteLine($"{profile.Name}, {profile.InchHeight}");
+        }
+    }
+}
