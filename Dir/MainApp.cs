574f3e731b8d430fd6e27f87ecc154ec3752d40f
Jinah-engineer <jeeeengineer@gmail.com>
2023-05-16 23:57:39 +0900

    [2023.05.16] File, Directory 1. 정보 조회 2. 생성

---
+++ Dir/MainApp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dir
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
+
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine("- Directoreis : ");
+
+            var directories = (from dir in Directory.GetDirectories(directory) // 하위 디렉터리 목록 조회
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                    Name = info.Name,
+                                    Attributes = info.Attributes
+                               }
+                               ).ToList();
+
+            foreach (var d in directories)
+                Console.WriteLine($"{d.Name} : {d.Attributes}");
+
+            Console.WriteLine("- Files :");
+
+            var files = (from file in Directory.GetFiles(directory) // 하위 파일 목록 조회
+                         let info = new FileInfo(file)
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         }).ToList();
+
+            foreach (var f in files)
+                Console.WriteLine(
+                    $"{f.Name} : {f.FileSize}, {f.Attributes}");
+        }
+    }
+}
