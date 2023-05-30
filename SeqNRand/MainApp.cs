595aceae70aca9db2b62a821d346c7aeeb3e15e5
Jinah-engineer <jeeeengineer@gmail.com>
2023-05-31 00:53:21 +0900

    [2023.05.31]

---
+++ SeqNRand/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SeqNRand
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Stream outStream = new FileStream("a.data", FileMode.Create);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x01);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x02);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x03);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Seek(5, SeekOrigin.Current);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x04);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Close();
+        }
+    }
+}
